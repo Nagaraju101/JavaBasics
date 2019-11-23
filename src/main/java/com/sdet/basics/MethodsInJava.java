f77f88865b2094045be5a8f489259b11904ae30d
Nagaraju Ankamreddi <Sanju@Nagarajus-MacBook-Pro.local>
2019-11-22 19:29:43 -0700

    new method added

--- src/main/java/com/sdet/basics/MethodsInJava.java
+++ src/main/java/com/sdet/basics/MethodsInJava.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 
 public class MethodsInJava 
 {
+	//1. No input no return
+	
+	public void test() {
+		System.out.println("No input or No return method");
+	}
 	
 	
-	//No input but return some value
+	//2. No input but return some value
 	public int sum() 
 	{
 		System.out.println("Sum Method");
@@ -25,5 +30,33 @@ public class MethodsInJava
 		arList.add("naga");
 		return arList;
 	}
-
+	
+	//3. Giving input and return some value
+	public int devision(int x , int y )
+	{
+		x = 44;
+		y = 56; 
+		int div = y/x;
+		return div ;
+	}
+	
+	//4. Multiple return statement
+	public String getContryName(String cityName)
+	{
+		System.out.println("Get city name");
+		if ( cityName.equalsIgnoreCase("Hyderabad"))
+		{
+			return "India";
+		}else if ( cityName.equalsIgnoreCase("London"))
+		{
+			return "UK";
+		}else if (cityName.equalsIgnoreCase("Denver"))
+		{
+			return "USA";
+		}else {
+			return cityName + ". The city name will be added soon";
+			
+		}
+	}
+	
 }
