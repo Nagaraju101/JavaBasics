9d1ae996b36702b50586d3838504b6e79b65d3ce
Nagaraju Ankamreddi <Sanju@Nagarajus-MacBook-Pro.local>
2019-11-24 21:34:22 -0700

    new method added

--- src/test/java/com/sdet/basics/TestingHere.java
+++ src/test/java/com/sdet/basics/TestingHere.java
@@ -9,11 +9,14 @@ public class TestingHere
     public static void main(String[] args){
     	
     	System.out.println("This is for SDET purpose  : ");	
+    	
         StringConcept stringConcat = new StringConcept();
         ForLoopConcept loopConcept = new ForLoopConcept();
         ArrayConcept arrayConcept = new ArrayConcept();
         MethodsInJava obj  = new MethodsInJava();
         IncrementDecrementConcept incrementDecrement = new IncrementDecrementConcept();
+        StaticNonStatic staticNonStatic = new StaticNonStatic();
+        
         System.out.println(stringConcat.getA() + stringConcat.getB());
         System.out.println(stringConcat.getC()+ stringConcat.getD());
     
@@ -25,11 +28,15 @@ public class TestingHere
         System.out.println(arrayConcept.doublArray());
         System.out.println(arrayConcept.stringArray());
         System.out.println(arrayConcept.objectArray());
-        System.out.println(incrementDecrement.incrementDecrement());
         
+        obj.test();
+        obj.getStudentNames();
         System.out.println(obj.devision(55, 7));
         System.out.println(obj.getCountryName("Denver"));
-        System.out.println(obj.getStudentNames());
+         
+        
+        System.out.println(incrementDecrement.incrementDecrement());
+        
         ArrayList<String> s = obj.getStudentNames();
         for (int j = 0; j < s.size() ; j++ ) 
         {
@@ -43,7 +50,11 @@ public class TestingHere
         	System.out.println(marks[i]);
         }
         
+        System.out.println(StaticNonStatic.age + " years and " + staticNonStatic.days + " days");
         
+        StaticNonStatic.distanceFromHome();
+        
+       
 
      
     }
