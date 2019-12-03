ddfac82318a79f81eda3aa06d21f08f29c3d10bd
Ankamreddi <Nagaraju.Ankamreddi@digitalglobe.com>
2019-12-03 13:18:47 -0700

    CollectionsframeWork

---
+++ src/main/java/com/sdet/basics/StringFunctions.java
@@ -0,0 +1,19 @@
+package com.sdet.basics;
+
+import java.util.Date;
+
+public class StringFunctions 
+{
+	
+	public void stringUsages()
+	{
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		Date date = new Date();
+		System.out.println(date);
+	}
+
+}
